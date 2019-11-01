5bc3068a224afcc3c1be37bb34224cf290782431
Gokhan Yigit <yigitgokhan1@gmail.com>
2019-11-01 10:03:40 +0100

    from terminal

--- selfStudy/src/replit/ConvertDecimalToBinary.java
+++ selfStudy/src/replit/ConvertDecimalToBinary.java
@@ -12,67 +12,39 @@ public class ConvertDecimalToBinary {
 	    for (int i = 0; i < binary.length; i++) {
 			binary[i]=0;
 	    }	
-    		
-	    	if(((decimal/128)%128)%2==1){  //128 //128
-	    		binary[0]=1;
-			}
-			if(((decimal/64)%64)%2==1) { //192 //64
-				binary[1]=1;
-			}
-			if(((decimal/32)%32)%2==1) { //224 //32
-				binary[2]=1;
-			}
-			if(((decimal/16)%16)%2==1) { //240 //16
-				binary[3]=1;
-			}
-			if(((decimal/8)%8)%2==1) { //248  //8
-				binary[4]=1;
-			}
-			if(((decimal/4)%4)%2==1) { //252 //4
-				binary[5]=1;
-			}
-			if(((decimal/2)%2)%2==1) {//254  /2
-				binary[6]=1;
-			}
-			if(((decimal%2))%2!=0) { //255  /1
-				binary[7]=1;
-			}
-		
+	    int pow=8;
+	    int pow2=7;
+    	for (int i = 0; i < binary.length; i++) {
+			binary[i]= decimal%(int)Math.pow(2, pow--)/(int)Math.pow(2, pow2--);
+		}
 	    System.out.println(Arrays.toString(binary));
 	    input.close();
 	}
-//	remainder=(decimal/128)%128;	
-//	if(remainder%2==1){  //128 //128
+//	if(((decimal/128)%128)%2==1){  //128 //128
 //		binary[0]=1;
 //	}
-//	remainder=(decimal/64)%64;
-//	if(remainder%2==1) { //192 //64
+//	if(((decimal/64)%64)%2==1) { //192 //64
 //		binary[1]=1;
 //	}
-//	
-//	remainder=(decimal/32)%32;
-//	if(remainder%2==1) { //224 //32
+//	if(((decimal/32)%32)%2==1) { //224 //32
 //		binary[2]=1;
 //	}
-//	remainder=(decimal/16)%16;
-//	if(remainder%2==1) { //240 //16
+//	if(((decimal/16)%16)%2==1) { //240 //16
 //		binary[3]=1;
 //	}
-//	remainder=(decimal/8)%8;
-//	if(remainder%2==1) { //248  //8
+//	if(((decimal/8)%8)%2==1) { //248  //8
 //		binary[4]=1;
 //	}
-//	remainder=(decimal/4)%4;
-//	if(remainder%2==1) { //252 //4
+//	if(((decimal/4)%4)%2==1) { //252 //4
 //		binary[5]=1;
 //	}
-//	remainder=(decimal/2)%2;
-//	if(remainder%2==1) {//254  /2
+//	if(((decimal/2)%2)%2==1) {//254  /2
 //		binary[6]=1;
 //	}
-//	remainder=(decimal%2);
-//	if(remainder%2!=0) { //255  /1
+//	if(((decimal%2))%2!=0) { //255  /1
 //		binary[7]=1;
 //	}
+	
+	
 
 }
